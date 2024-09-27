fdb4318c5536f175625cb3aba357e0c96f1c1250
roby-avo <r.avogadro@campus.unimib.it>
2024-09-27 21:30:22 +0200

    Update spacy version requirement to 3.0 in requirements.txt and remove newline at end of file
    Refactor setup.py to update package name and remove Python 3.7 classifier

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='column_classifier',  # This is the actual package name
+    name='column_classifier',
     version='0.1.0',
     description='A column classifier using spaCy for entity recognition.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Roberto',
     author_email='roberto.avogadro@sintef.no',
-    url='https://github.com/roby-avo/spacy-column-classifier',  # Keep your GitHub URL
+    url='https://github.com/roby-avo/spacy-column-classifier',
     license='Apache License 2.0',
-    packages=find_packages(),  # Automatically finds the 'column_classifier' package
+    packages=find_packages(),  # Automatically finds your 'column_classifier' package
     install_requires=[
         'spacy',  # Add other dependencies here
     ],
@@ -19,7 +19,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
     ],
     python_requires='>=3.7',
 )
