99bab0d0e867263eecf5925eb7b51f9d822785cc
roby-avo <r.avogadro@campus.unimib.it>
2024-09-27 21:36:01 +0200

    Refactor package structure and remove unused code

--- column_classifier/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py for the spacy_column_classifier package
-
-# Import the ColumnClassifier class so it can be accessed directly
-from .column_classifier import ColumnClassifier
-
-# Specify what is exported when users do 'from spacy_column_classifier import *'
-__all__ = ['ColumnClassifier']
